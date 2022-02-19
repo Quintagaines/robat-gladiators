aec9a39f989d091dd499a312b2f2ae1d89cbad61
Quinta Gaines <qgaines2009@gmail.com>
2022-02-18 19:31:23 -0500

    variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,29 @@
-window.alert("This is an alert! JavaScript is running!");
+var playName = window.prompt("what is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+// You can also log multiple values at once like this 
+
+console.log (playName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+
+
+//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+enemyHealth = enemyHealth - playerAttack
+console.log(
+    playName + "attacked" + enemyName + ". "+" now has " + enemyHealth + "health remaining."
+)
+
+  // Log a resulting message to the console so we know that it worked.
+
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+
+  // Log a resulting message to the console so we know that it worked.
+};
+
+fight();
